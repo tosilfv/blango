d4027bd48d252dc4d820d382d088387b1cd8d028
Toni Silfver <toni.silfver@metropolia.fi>
2023-12-06 05:11:15 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect 
 from django.utils import timezone
 from blog.models import Post
-from django.shortcuts import redirect
 from blog.forms import CommentForm
 import logging
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 logger = logging.getLogger(__name__)
 
 # Create your views here.
+# @cache_page(300)
+# @vary_on_cookie
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
-    #posts = Post.objects.all()
-    #print('posts: ', posts)
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
+    # from django.http import HttpResponse
+    # logger.debug("Index function is called!")
+    # return HttpResponse(str(request.user).encode("ascii"))
+    # posts = Post.objects.filter(published_at__lte=timezone.now())
+    # logger.debug("Got %d posts", len(posts))
+    # return render(request, "blog/index.html", {"posts": posts})
+    # posts = Post.objects.filter(published_at__lte=timezone.now())
+    # #posts = Post.objects.all()
+    # #print('posts: ', posts)
+    # logger.debug("Got %d posts", len(posts))
+    # return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
