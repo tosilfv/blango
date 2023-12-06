e56080e3b48dfc95b4f85700310972fe8c353f42
Toni Silfver <toni.silfver@metropolia.fi>
2023-12-06 07:54:14 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -63,6 +63,7 @@ class Dev(Configuration):
       'django.contrib.sessions',
       'django.contrib.messages',
       'django.contrib.staticfiles',
+      "blango_auth",
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
@@ -198,6 +199,7 @@ class Dev(Configuration):
   }
 
   INTERNAL_IPS = ["192.168.10.156"]
+  AUTH_USER_MODEL = "blango_auth.User"
 
 class Prod(Dev):
     DEBUG = False
