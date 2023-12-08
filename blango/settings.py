51b29388df835f9f609926e4fd01edfa10ad0219
Toni Silfver <toni.silfver@metropolia.fi>
2023-12-08 08:16:38 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -68,6 +68,7 @@ class Dev(Configuration):
       "allauth.socialaccount.providers.google",
       "rest_framework",
       "rest_framework.authtoken",
+      "drf_yasg",
   ]
 
   MIDDLEWARE = [
@@ -218,6 +219,13 @@ class Dev(Configuration):
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
+  }
+
+  SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+      "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+      "Basic": {"type": "basic"},
+    }
   }
 
 class Prod(Dev):
