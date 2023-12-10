2bf1aa8820da7363d23dc0c12f75524ab8a9549c
Toni Silfver <toni.silfver@metropolia.fi>
2023-12-10 09:20:43 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from blog.forms import CommentForm
 import logging
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_cookie
+from django.urls import reverse
 
 logger = logging.getLogger(__name__)
 
@@ -57,4 +58,6 @@ def get_ip(request):
   return HttpResponse(request.META['REMOTE_ADDR'])
 
 def post_table(request):
-  return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
