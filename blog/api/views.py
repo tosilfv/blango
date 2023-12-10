f16332e1ee14a104fb090fdcc4fb9af5e020d068
Toni Silfver <toni.silfver@metropolia.fi>
2023-12-10 06:57:46 +0000

    Finish pagination and django filter

--- blog/api/views.py
+++ blog/api/views.py
@@ -24,6 +24,8 @@ from django.utils import timezone
 from datetime import timedelta
 from django.http import Http404
 
+from blog.api.filters import PostFilterSet
+
 # class PostList(generics.ListCreateAPIView):
 #     queryset = Post.objects.all()
 #     serializer_class = PostSerializer
@@ -35,6 +37,9 @@ from django.http import Http404
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    # filterset_fields = ["author", "tags"]
+    ordering_fields = ["published_at", "author", "title", "slug"]
+    filterset_class = PostFilterSet
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
 
@@ -51,6 +56,13 @@ class PostViewSet(viewsets.ModelViewSet):
         if request.user.is_anonymous:
             raise PermissionDenied("You must be logged in to see which Posts are yours")
         posts = self.get_queryset().filter(author=request.user)
+
+        page = self.paginate_queryset(posts)
+
+        if page is not None:
+            serializer = PostSerializer(page, many=True, context={"request": request})
+            return self.get_paginated_response(serializer.data)
+
         serializer = PostSerializer(posts, many=True, context={"request": request})
         return Response(serializer.data)
     
@@ -59,39 +71,39 @@ class PostViewSet(viewsets.ModelViewSet):
         return super(PostViewSet, self).list(*args, **kwargs)
     
     def get_queryset(self):
-    if self.request.user.is_anonymous:
-        # published only
-        queryset = self.queryset.filter(published_at__lte=timezone.now())
+        if self.request.user.is_anonymous:
+            # published only
+            queryset = self.queryset.filter(published_at__lte=timezone.now())
 
-    elif not self.request.user.is_staff:
-        # allow all
-        queryset = self.queryset
-    else:
-        queryset = self.queryset.filter(
-            Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
-        )
+        elif not self.request.user.is_staff:
+            # allow all
+            queryset = self.queryset
+        else:
+            queryset = self.queryset.filter(
+                Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
+            )
 
-    time_period_name = self.kwargs.get("period_name")
+        time_period_name = self.kwargs.get("period_name")
 
-    if not time_period_name:
-        # no further filtering required
-        return queryset
+        if not time_period_name:
+            # no further filtering required
+            return queryset
 
-    if time_period_name == "new":
-        return queryset.filter(
-            published_at__gte=timezone.now() - timedelta(hours=1)
-        )
-    elif time_period_name == "today":
-        return queryset.filter(
-            published_at__date=timezone.now().date(),
-        )
-    elif time_period_name == "week":
-        return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
-    else:
-        raise Http404(
-            f"Time period {time_period_name} is not valid, should be "
-            f"'new', 'today' or 'week'"
-        )
+        if time_period_name == "new":
+            return queryset.filter(
+                published_at__gte=timezone.now() - timedelta(hours=1)
+            )
+        elif time_period_name == "today":
+            return queryset.filter(
+                published_at__date=timezone.now().date(),
+            )
+        elif time_period_name == "week":
+            return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
+        else:
+            raise Http404(
+                f"Time period {time_period_name} is not valid, should be "
+                f"'new', 'today' or 'week'"
+            )
 
     @method_decorator(cache_page(120))
     @method_decorator(vary_on_headers("Authorization", "Cookie"))
@@ -114,6 +126,12 @@ class TagViewSet(viewsets.ModelViewSet):
     @action(methods=["get"], detail=True, name="Posts with the Tag")
     def posts(self, request, pk=None):
         tag = self.get_object()
+        page = self.paginate_queryset(tag.posts)
+        if page is not None:
+            post_serializer = PostSerializer(
+                page, many=True, context={"request": request}
+            )
+            return self.get_paginated_response(post_serializer.data)
         post_serializer = PostSerializer(
             tag.posts, many=True, context={"request": request}
         )
